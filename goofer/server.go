6784759ac1ae9286ec7010fa700cde7da6030a1f
tacheraSasi <tacherasasi@gmail.com>
2025-08-25 12:46:40 +0300

    Update application title to "MockWails" and add Server struct definition

---
+++ goofer/server.go
@@ -0,0 +1,11 @@
+package goofer
+
+type Server struct {
+	ID   uint   `orm:"primaryKey;autoIncrement" validate:"required"`
+	Name string `orm:"type:varchar(255);notnull" validate:"required"`
+	Host string `orm:"type:varchar(255);notnull" validate:"required"`
+	Port uint   `orm:"type:int;notnull" validate:"required"`
+	RequestCount uint `orm:"type:int;default:0;notnull" validate:"required"`
+	ResponseStructure string `orm:"type:text;notnull" validate:"required"`
+	RequestStructure string `orm:"type:text;notnull" validate:"required"`
+}
