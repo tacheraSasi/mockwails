33796bd124e5570f1a65626ec06da0ffab166507
tacheraSasi <tacherasasi@gmail.com>
2025-08-26 14:24:03 +0300

    Implement GetClient function for database connection and client initialization

--- goofer/db.go
+++ goofer/db.go
@@ -1,5 +1,35 @@
 package goofer
 
+import (
+	"database/sql"
+	"log"
+
+	"github.com/bluesky-social/indigo/models"
+	"github.com/gooferOrm/goofer/dialect"
+	"github.com/gooferOrm/goofer/engine"
+)
+
+func GetClient(models ...interface{}) (*engine.Client, error) {
+	db, err := sql.Open("sqlite3", "./db.db")
+    if err != nil {
+        log.Fatalf("open db: %v", err)
+		return nil, err
+    }
+    defer db.Close()
+
+    // engine setup:
+    gooferClient, err := engine.NewClient(
+        db,
+        dialect.NewSQLiteDialect(), 
+        models...,
+    )
+    if err != nil {
+        log.Fatalf("engine init: %v", err)
+		return nil, err
+    }
+	return gooferClient, nil
+}
+
 func CreateServer() error {
 	// TODO: Implementation for creating a server in the database
 	return nil
