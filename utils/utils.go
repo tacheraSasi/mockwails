7a3cae05fcf0b3115cc8cac2e4e10afdf07f45d5
tacheraSasi <tacherasasi@gmail.com>
2025-08-27 12:30:43 +0300

    Format IsPortInUse function for consistency and readability

--- utils/utils.go
+++ utils/utils.go
@@ -3,13 +3,11 @@ package utils
 import (
 	"fmt"
 	"net"
-
-	"github.com/jaypipes/ghw/pkg/pci/address"
 )
 
-func IsPortInUse(port int)bool{
-	address := fmt.Sprintf(":%d",port)
-	ln, err := net.Listen("tcp",address)
+func IsPortInUse(port int) bool {
+	address := fmt.Sprintf(":%d", port)
+	ln, err := net.Listen("tcp", address)
 	if err != nil {
 		return false
 	}
