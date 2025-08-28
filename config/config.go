cc43462feee33ea514fbb3b291ee51abb82e7006
Tachera Sasi <tacherasasi@gmail.com>
2025-08-28 09:39:56 +0300

    Ellie: add config package

---
+++ config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	once sync.Once
+)
+
+type Config struct {
+	
+}
+
+func init(){
+	fmt.Println("Config initialized...")
+	once.Do(func(){
+		// TODO: Create a folder mockwails in the $HOME directory of the user
+	})
+}
+
+func GetConfig() *Config {
+	return &Config{}
+}
