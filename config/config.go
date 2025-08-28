451fe70c9fb0ed8f6727049ac2df1b267758d14f
Tachera Sasi <tacherasasi@gmail.com>
2025-08-28 21:15:07 +0300

    Refactor Config struct and initialization to ensure proper directory creation and OS type detection

--- config/config.go
+++ config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"fmt"
+	"os"
+	"runtime"
 	"sync"
 )
 
@@ -10,16 +12,44 @@ var (
 )
 
 type Config struct {
-	
+	ConfigPath string
+	DBPath     string
+	DebugMode  bool
+	OsType     string
 }
 
-func init(){
+func init() {
 	fmt.Println("Config initialized...")
-	once.Do(func(){
+	once.Do(func() {
 		// TODO: I will Create a folder mockwails in the $HOME directory of the user
+		config := GetConfig()
+		os.MkdirAll(config.ConfigPath, os.ModePerm)
 	})
 }
 
 func GetConfig() *Config {
-	return &Config{}
+	configPath, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	configPath += "/.mockwails"
+	return &Config{
+		ConfigPath: configPath,
+		DBPath:     configPath + "/db.sqlite",
+		DebugMode:  false,
+		OsType:     getOsType(),
+	}
+}
+
+func getOsType() string {
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		return "macOS"
+	case "linux":
+		return "Linux"
+	case "windows":
+		return "Windows"
+	default:
+		return "Unknown"
+	}
 }
