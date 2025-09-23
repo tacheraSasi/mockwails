d996303afc97c5c6ab6c759bc576f135c13604b2
Tachera Sasi <tacherasasi@gmail.com>
2025-09-23 10:22:55 +0300

    Refactor binary management in cmd.go: update paths, improve error messages, and ensure executable permissions

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -8,13 +8,23 @@ import (
 // For now i want to move the bin to a generic location
 // and then call it from the main app
 
-func main(){
+func main() {
 	binName := "mockwails"
-	binPath := "../build/bin/MockWails.app/Contents/MacOS/"+binName
-	err := exec.Command("chmod", "+x", binPath, "&&", "sudo movebin", binPath).Run()
+	binPath := "../build/bin/mockwails.app/Contents/MacOS/mockwails"
+	destPath := "/usr/local/bin/" + binName
+
+	// Making the binary executable
+	err := exec.Command("chmod", "+x", binPath).Run()
 	if err != nil {
-		log.Fatalf("Failed to start mockwails: %v", err)
+		log.Fatalf("Failed to make binary executable: %v", err)
 	}
-	log.Printf("Successfully moved %s to /usr/local/bin/%s", binName, binName)
+
+	// Copying the binary to /usr/local/bin
+	err = exec.Command("sudo", "cp", binPath, destPath).Run()
+	if err != nil {
+		log.Fatalf("Failed to copy mockwails to /usr/local/bin: %v", err)
+	}
+
+	log.Printf("Successfully copied %s to %s", binName, destPath)
 	log.Printf("You can now run the application using the command: %s", binName)
 }
